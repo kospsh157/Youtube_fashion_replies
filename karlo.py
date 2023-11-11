cc106d09be0cadbcac7f1c730c6b59987842171f
psh <kospsh157@gmail.com>
2023-11-11 12:36:50 +0900

    update about LDA

--- karlo.py
+++ karlo.py
@@ -3,6 +3,7 @@ import requests
 import json
 import urllib
 from PIL import Image
+from making_prompt import making_prompt
 
 # [내 애플리케이션] > [앱 키] 에서 확인한 REST API 키 값 입력
 REST_API_KEY = 'ada696dfddc16d1227e321d7574f06c1'
@@ -10,19 +11,14 @@ REST_API_KEY = 'ada696dfddc16d1227e321d7574f06c1'
 # 이미지 생성하기 요청
 
 # 칼로 불용어 설정
-negative_prompt = "news paper, sleeping cat, dog, ugly face, cropped, foods, alphabet, latters, Home Appliances, Electronics, Furniture, Beverages, Tools, \
+negative_prompt = "news paper, children, old people, elder people, baby, sleeping cat, dog, ugly face, cropped, foods, alphabet, latters, Home Appliances, Electronics, Furniture, Beverages, Tools, \
     Toys, Books, Music items, sports equipment, pet supplies, gardening supplies, artwork, office supplies, building materials, outdoor activity gear"
 
 
-def t2i():
-    # combined_srt = ", ".join(fashion_items)
-    # prompt = "Generate a realistic image of a well-groomed male model on a simple urban street, showcasing " + \
-    #     ", ".join(fashion_items) + ". The items should be sharply in focus, capturing their texture and fit in a real-world setting. The model's appearance is natural and photorealistic, with a poised demeanor that complements the understated street scene. The lighting and composition should resemble a high-quality photograph, avoiding any cartoonish or cybernetic aesthetic, to truly emphasize the fashion items in a contemporary, believable manner."
-    # # prompt += combined_srt
-    # prompt = "On a realistic cool background, a model in a cool pose is wearing " + \
-    #     ", ".join(fashion_items)
-
-    prompt = "A stylish model is wearing a vintage-style denim jacket. The jacket is decorated with a check pattern and is layered with a slim fit white t-shirt underneath. The model is wearing a comfortable jumper and a casual cap on her head. Overall, this combination expresses a classic yet modern sensibility, and the long sleeves of the jacket and the slim silhouette of the jumper stand out in particular."
+def t2i(keywords):
+    list_of_keywords = ", ".join(keywords)
+    prompt = making_prompt(list_of_keywords)
+    # prompt = "A stylish model is wearing a vintage-style denim jacket. The jacket is decorated with a check pattern and is layered with a slim fit white t-shirt underneath. The model is wearing a comfortable jumper and a casual cap on her head. Overall, this combination expresses a classic yet modern sensibility, and the long sleeves of the jacket and the slim silhouette of the jumper stand out in particular."
 
     r = requests.post(
         'https://api.kakaobrain.com/v2/inference/karlo/t2i',
@@ -40,13 +36,10 @@ def t2i():
     return response
 
 
-# 프롬프트에 사용할 제시어
+# # 이미지 생성하기 REST API 호출
+# response = t2i()
 
-
-# 이미지 생성하기 REST API 호출
-response = t2i()
-
-# 응답의 첫 번째 이미지 생성 결과 출력하기
-result = Image.open(urllib.request.urlopen(
-    response.get("images")[0].get("image")))
-result.show()
+# # 응답의 첫 번째 이미지 생성 결과 출력하기
+# result = Image.open(urllib.request.urlopen(
+#     response.get("images")[0].get("image")))
+# result.show()
--- making_prompt.py
+++ making_prompt.py
@@ -1,34 +1,20 @@
-import openai
-
+from openai import OpenAI
 
 # 한글로 약 1000개의 글자당, 0.12불 요금
 
-
-# OpenAI API 키 설정
-# 여기서 'your-api-key'를 실제 API 키로 대체하세요.
-
-# fasion_keywords = ['jacket', 'denim', 'vintage', 'checkered',
-#                    't-shirt', 'cap', 'jumper', 'slim', 'sleeve', 'white']
-# # GPT 모델을 사용하여 텍스트 생성 요청
-# response = openai.Completion.create(
-#     engine="text-davinci-003",  # 모델을 지정 (예: text-davinci-003)
-#     prompt=f"A stylish model is showcasing a combination of fashion items. The outfit includes {fasion_keywords}, creating a modern and sophisticated look. The ensemble is completed with accessories that enhance the overall style. Non-fashion items, if any, are not part of the attire.",
-#     max_tokens=2000  # 생성할 토큰의 최대 개수
-# )
-
-# # 생성된 텍스트 출력
-# print(response.choices[0].text.strip())
-
-
-# OpenAI API 키 설정
-# 여기서 'your-api-key'를 실제 API 키로 대체하세요.
-openai.api_key = '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'
-
-
-response = openai.Completion.create(
-    model="text-davinci-003",  # GPT-4 모델을 사용
-    prompt="이것은 테스트 메시지입니다.",  # 여기에 원하는 프롬프트를 넣으세요
-    max_tokens=50  # 생성할 토큰의 최대 수
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key="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",
 )
 
-print(response.choices[0].text.strip())
+
+def making_prompt(fashion_items):
+
+    res = client.completions.create(
+        model="text-davinci-003",
+        prompt=f"Here are some fashion-related keywords,{fashion_items}. I want you to use these keywords to create an image of a single model, fully dressed in these fashion items from head to toe, ensuring the entire outfit is visible and presented elegantly. The model should be an adult and the attire should be tasteful and appropriate, with no explicit or excessive exposure. If the styling is feminine, the model should be a woman; if the styling is masculine, the model should be a man. Please avoid featuring children or elderly models, and ensure the focus is on showcasing the fashion items respectfully.",
+        max_tokens=250,
+        temperature=0.5
+    )
+    print(res.choices[0].text.strip())
+    return res.choices[0].text.strip()
